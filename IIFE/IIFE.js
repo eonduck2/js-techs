0230c5d1ccbe708cd3deb4aaa34f21b6d7cfd4fb
eonduck2 <whdtnxd@gmail.com>
2024-06-05 14:17:09 +0900

    IIFE

--- IIFE/IIFE.js
+++ IIFE/IIFE.js
@@ -1,12 +1,13 @@
 const IIFE = (a, b) => {
   let first = a;
   const second = b;
-  console.log(first);
-  ((age) => {
-    console.log(age);
-    first = age;
-    console.log(first);
-  })(20);
+
+  ((callback) => {
+    // first++;
+    callback(first + second);
+  })((test) => {
+    console.log(test);
+  });
 };
 
 IIFE(1, 2);
