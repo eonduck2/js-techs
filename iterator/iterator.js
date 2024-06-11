97047e113423d52fec4abc01bac5ae78d8db9b34
eonduck2 <whdtnxd@gmail.com>
2024-06-11 11:19:40 +0900

    update files

--- iterator/iterator.js
+++ iterator/iterator.js
@@ -1,27 +1,37 @@
-let range = {
-  from: 1,
-  to: 5,
+// let range = {
+//   from: 1,
+//   to: 5,
 
-  // for..of 최초 호출 시, Symbol.iterator가 호출됩니다.
-  [Symbol.iterator]() {
-    // Symbol.iterator는 이터레이터 객체를 반환합니다.
-    // for..of는 반환된 이터레이터 객체만을 대상으로 동작하는데, 이때 다음 값도 정해집니다.
-    return {
-      current: this.from,
-      last: this.to,
+//   [Symbol.iterator]() {
+//     return {
+//       current: this.from,
+//       last: this.to,
 
-      // for..of 반복문에 의해 각 이터레이션마다 next()가 호출됩니다.
-      next() {
-        // next()는 객체 형태의 값, {done:.., value :...}을 반환해야 합니다.
-        if (this.current <= this.last) {
-          return { done: false, value: this.current++ };
-        } else {
-          return { done: true };
-        }
-      },
-    };
-  },
+//       next() {
+//         if (this.current <= this.last) {
+//           return { done: false, value: this.current++ };
+//         } else {
+//           return { done: true };
+//         }
+//       },
+//     };
+//   },
+// };
+
+// console.log([...range]);
+
+// -------------------------------------------------
+
+const testObj = {
+  test: `테스트`,
+  test2: `테스트2`,
+  test3: `테스트3`,
 };
 
-// 객체 range를 대상으로 하는 이터레이션은 range.from과 range.to 사이의 숫자를 출력합니다.
-console.log([...range]); // 1,2,3,4,5
+testObj[Symbol.iterator] = function* () {
+  yield this.test;
+  yield this.test2;
+  yield this.test3;
+};
+
+console.log([testObj]);
